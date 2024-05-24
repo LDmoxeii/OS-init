19219e88638d6aa8c02a13a22b5a154f05446689
LD_moxeii <2649075705@qq.com>
2024-05-24 17:24:17 +0800

    fix

---
+++ windows/config/RimeData/rime.lua
@@ -0,0 +1,7 @@
+-- select_character_processor: 以词定字
+-- 详见 `lua/select_character.lua`
+select_character_processor = require("select_character")
+time_translator = require("time")
+date_translator = require("date")
+week_translator = require("week")
+easy_en_translator = require("easy_en")
